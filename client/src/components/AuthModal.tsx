dcfb066231d0cbe008f758c98871d5866010dbe3
jatinkumar-me <jatin208015@gmail.com>
2023-07-10 20:32:33 +0530

    Added lazy loading for Authentication form

--- client/src/components/AuthModal.tsx
+++ client/src/components/AuthModal.tsx
@@ -1,13 +1,15 @@
 import { ContextModalProps } from "@mantine/modals";
-import AuthForm from "./AuthForm";
+import { Suspense, lazy } from "react";
+
+const AuthForm = lazy(() => import("./AuthForm"));
 
 const AuthModal = ({
   context,
   id,
 }: ContextModalProps<{ modalBody: string }>) => (
-  <>
-      <AuthForm onClose={() => context.closeModal(id)}/>
-  </>
+  <Suspense fallback={<div>Loading...</div>}>
+    <AuthForm onClose={() => context.closeModal(id)} />
+  </Suspense>
 );
 
 export default AuthModal;
